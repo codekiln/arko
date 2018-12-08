23538dbf14bbec97e3a963babb37db7a0fb49a6f
Myer Nore <pnore@hbs.edu>
2018-12-08 16:40:01 -0500

    paintings and drawings have dedicated pages and navbar references them

---
+++ src/pages/drawings/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Link, graphql} from 'gatsby'
+import Layout from '../../components/Layout'
+import Img from "gatsby-image";
+
+export default class DrawingsIndexPage extends React.Component {
+    render() {
+        const {data} = this.props
+        const {edges: posts} = data.allMarkdownRemark
+
+        return (
+            <Layout>
+                <section className="section">
+                    <div className="container">
+                        {posts.map(({node: post}) => (
+                            <div
+                                className="content"
+                                style={{"max-width": 561}}
+                                key={post.id}
+                            >
+                                <Link className="has-text-primary" to={post.fields.slug}>
+                                    <Img fluid={post.frontmatter.image.childImageSharp.fluid} alt={post.title}/>
+                                </Link>
+                                <p>
+                                    <Link className="has-text-primary" to={post.fields.slug}>
+                                        {post.frontmatter.title}
+                                    </Link>
+                                    <span> &bull; </span>
+                                    <small>{post.frontmatter.date}</small>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </Layout>
+        )
+    }
+}
+
+DrawingsIndexPage.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
+    }),
+}
+
+export const pageQuery = graphql`
+query DrawingsIndexQuery {
+  allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {
+      frontmatter: {
+        templateKey: {eq: "artwork-post"}, category: {eq: "Drawings"}
+      }
+    }) {
+    edges {
+      node {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          date(formatString: "YYYY")
+          image {
+            id
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          medium
+          category
+        }
+      }
+    }
+  }
+}
+`
--- src/pages/works/woman-hero-self-portrait-as-the-virgin-and-child.md
+++ src/pages/works/woman-hero-self-portrait-as-the-virgin-and-child.md
@@ -5,6 +5,6 @@ image: /img/womanherolighterwebsite.jpg
 date: 2018-06-01T00:00:00.000Z
 medium: 'Oil, Lapis, and Gold on Panel'
 dimension: 16x12 inches
-category: Drawings
+category: Paintings
 ---
 
