85a7042fc0502eb42241b852ce6e642f7294823d
Myer Nore <pnore@hbs.edu>
2018-12-08 17:01:38 -0500

    add work page post

--- src/templates/artwork-post.js
+++ src/templates/artwork-post.js
@@ -1,74 +1,81 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
+import {kebabCase} from 'lodash'
 import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
+import {graphql, Link} from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Content, {HTMLContent} from '../components/Content'
+import Img from "gatsby-image";
 
 export const ArtworkPostTemplate = ({
-  content,
-  contentComponent,
-  description,
-  title,
-  helmet,
-}) => {
-  const PostContent = contentComponent || Content
+                                        content,
+                                        image,
+                                        contentComponent,
+                                        description,
+                                        title,
+                                        date,
+                                        helmet,
+                                    }) => {
+    const PostContent = contentComponent || Content
 
-  return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+    return (
+        <section className="section">
+            {helmet || ''}
+            <div className="container content">
+                <div className="columns">
+                    <div className="column is-10 is-offset-1">
+                        <Img fluid={image.childImageSharp.fluid} alt={title} style={{"max-width": 561}}/>
+                        <p className="work-details margin-bottom-0">{title}</p>
+                        <p className="margin-top-0 margin-bottom-0">{date}</p>
+                        <p className="margin-top-0">{description}</p>
+                        <PostContent content={content}/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 ArtworkPostTemplate.propTypes = {
-  content: PropTypes.node.isRequired,
-  contentComponent: PropTypes.func,
-  description: PropTypes.string,
-  title: PropTypes.string,
-  helmet: PropTypes.object,
+    content: PropTypes.node.isRequired,
+    image: PropTypes.node.isRequired,
+    contentComponent: PropTypes.func,
+    description: PropTypes.string,
+    title: PropTypes.string,
+    date: PropTypes.string,
+    helmet: PropTypes.object,
 }
 
-const ArtworkPost = ({ data }) => {
-  const { markdownRemark: post } = data
+const ArtworkPost = ({data}) => {
+    const {markdownRemark: post} = data
 
-  return (
-    <Layout>
-      <ArtworkPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
+    return (
+        <Layout>
+            <ArtworkPostTemplate
+                content={post.html}
+                contentComponent={HTMLContent}
+                description={post.frontmatter.description}
+                helmet={
+                    <Helmet titleTemplate="%s | Blog">
+                        <title>{`${post.frontmatter.title}`}</title>
+                        <meta
+                            name="description"
+                            content={`${post.frontmatter.description}`}
+                        />
+                    </Helmet>
+                }
+                image={post.frontmatter.image}
+                title={post.frontmatter.title}
+                date={post.frontmatter.date}
             />
-          </Helmet>
-        }
-        title={post.frontmatter.title}
-      />
-    </Layout>
-  )
+        </Layout>
+    )
 }
 
 ArtworkPost.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.object,
+    }),
 }
 
 export default ArtworkPost
@@ -77,11 +84,22 @@ export const pageQuery = graphql`
   query ArtworkPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
-      html
+      fields {
+          slug
+        }
       frontmatter {
-        date(formatString: "YYYY")
         title
-        description
+        date(formatString: "YYYY")
+        image {
+          id
+          childImageSharp {
+            fluid(maxWidth: 561, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        medium
+        category
       }
     }
   }
