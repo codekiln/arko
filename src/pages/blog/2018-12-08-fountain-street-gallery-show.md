5080fff9a67810414a5ec96ab231df45bbbcdb7e
Myer Nore <pnore@hbs.edu>
2018-12-10 20:37:46 -0500

    add a blog section to the navbar and clean up content

---
+++ src/pages/blog/2018-12-08-fountain-street-gallery-show.md
@@ -0,0 +1,16 @@
+---
+templateKey: blog-post
+title: Fountain Street Gallery Show
+date: 2018-12-08T15:24:05.637Z
+description: Come see my work in Boston this January!
+tags:
+  - shows
+---
+[*Self Portrait as a Beautiful Lady*](http://localhost:8000/works/self-portrait-as-a-beautiful-lady/) 
+will be available at [Fountain Street Gallery's January Exhibition *The Depth of Perception*](https://www.fsfaboston.com/1111-the-depth-of-perception), 
+a national juried show. Don't miss this opportunity to see my work up close! 
+
+<br />
+
+<a href="https://www.fsfaboston.com/1111-the-depth-of-perception" target="_blank">![The Depth of Perception](/img/TheDepthofPerception_DigitalPostcard.jpg)</a>
+
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,42 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
+import { HTMLContent } from '../../components/Content'
 
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
+    // noinspection CheckTagEmptyBody
     return (
       <Layout>
         <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+          <div className="columns is-centered">
+            <div className="column is-three-fifths">
+              {posts.map(({ node: post }) => (
+                <div className="box" key={post.id}>
+                  <div className="content">
+                    <h2 className="is-size-4 is-bold-light">
+                      <Link to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                    <span> &bull; </span>
+                    <small><em>{post.frontmatter.description}</em></small>
+                  </div>
+                  <hr />
+
+                  <HTMLContent content={post.html} />
+                </div>
+              ))}
             </div>
-            {posts.map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #333', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
           </div>
         </section>
       </Layout>
@@ -61,14 +56,16 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt(pruneLength: 400)
+          excerpt(pruneLength: 600)
           id
+          html
           fields {
             slug
           }
           frontmatter {
             title
             templateKey
+            description
             date(formatString: "MMMM DD, YYYY")
           }
         }
