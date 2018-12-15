0f4bc9ecfc4e66b19ae3bcfdcc7566af31405229
Myer Nore <pnore@hbs.edu>
2018-12-15 10:16:47 -0500

    use recaptcha in contact page and center navbar
    * format thanks page
    * add react-google-recaptcha as dependency
    *

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { navigate } from 'gatsby-link'
+import { navigateTo } from 'gatsby-link'
 import Layout from '../../components/Layout'
+import Recaptcha from 'react-google-recaptcha'
+
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY
 
 function encode(data) {
   return Object.keys(data)
@@ -8,14 +11,18 @@ function encode(data) {
     .join('&')
 }
 
-export default class Index extends React.Component {
+export default class Contact extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { isValidated: false }
+    this.state = {}
   }
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleRecaptcha = value => {
+    this.setState({ 'g-recaptcha-response': value })
   }
 
   handleSubmit = e => {
@@ -29,7 +36,7 @@ export default class Index extends React.Component {
         ...this.state,
       }),
     })
-      .then(() => navigate(form.getAttribute('action')))
+      .then(() => navigateTo(form.getAttribute('action')))
       .catch(error => alert(error))
   }
 
@@ -37,75 +44,88 @@ export default class Index extends React.Component {
     return (
       <Layout>
         <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={this.handleChange}
-                      id={'name'}
-                      required={true}
-                    />
+          <div className="columns is-centered">
+            <div className="column is-two-fifths">
+              <div className="content">
+                <h1>Contact</h1>
+                <form
+                  name="contact"
+                  method="post"
+                  action="/contact/thanks/"
+                  data-netlify="true"
+                  data-netlify-recaptcha="true"
+                  data-netlify-honeypot="are-you-friend"
+                  onSubmit={this.handleSubmit}
+                >
+                  {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div hidden>
+                    <label>
+                      {/* This is a bot detector. If a bot enters input in this, it will be discarded. */}
+                      Are you a friend?{' '}
+                      <input
+                        name="are-you-friend"
+                        onChange={this.handleChange}
+                      />
+                    </label>
                   </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={this.handleChange}
-                      id={'email'}
-                      required={true}
-                    />
+                  <div className="field">
+                    <label className="label" htmlFor={'name'}>
+                      Your name
+                    </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type={'text'}
+                        name={'name'}
+                        onChange={this.handleChange}
+                        id={'name'}
+                        required={true}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={this.handleChange}
-                      id={'message'}
-                      required={true}
-                    />
+                  <div className="field">
+                    <label className="label" htmlFor={'email'}>
+                      Email
+                    </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type={'email'}
+                        name={'email'}
+                        onChange={this.handleChange}
+                        id={'email'}
+                        required={true}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+                  <div className="field">
+                    <label className="label" htmlFor={'message'}>
+                      Message
+                    </label>
+                    <div className="control">
+                      <textarea
+                        className="textarea"
+                        name={'message'}
+                        onChange={this.handleChange}
+                        id={'message'}
+                        required={true}
+                      />
+                    </div>
+                  </div>
+                  <Recaptcha
+                    className="field"
+                    ref="recaptcha"
+                    sitekey={RECAPTCHA_KEY}
+                    onChange={this.handleRecaptcha}
+                  />
+                  <div className="field">
+                    <button className="button is-link" type="submit">
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </section>
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -4,16 +4,22 @@ import Layout from '../../components/Layout'
 export default () => (
   <Layout>
     <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>
-            For more information, please also see my
-            <a href="https://www.facebook.com/ArhiaKohlmoosArt" target="_blank" rel="noopener noreferrer">
-              Facebook Artist Page
-            </a>
-            .
-          </p>
+      <div className="columns is-centered">
+        <div className="column is-two-fifths">
+          <div className="content">
+            <h1>Thank you!</h1>
+            <p>
+              For more information, please also see my{' '}
+              <a
+                href="https://www.facebook.com/ArhiaKohlmoosArt"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Facebook Artist Page
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </section>
